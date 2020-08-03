fdde5eb041ba684578d4c446fa18c82ccbd83631
MikeKravchenko <kravchenko.soft@gmail.com>
2020-08-03 19:11:24 -0400

    ds

--- lol.py
+++ lol.py
@@ -1,9 +1,16 @@
 import os
-def create_python_script(filename):
-  comments = "# Start of a new Python program"
-  with open(filename, "w") as file:
-    file.write(comments)
-    filesize = os.path.getsize(ss)
-  return(filesize)
 
-print(create_python_script("program.py")) 
+def new_directory(directory, filename):
+  # Before creating a new directory, check to see if it already exists
+  if os.path.isdir(directory) == False:
+    os.mkdir(directory)
+
+  # Create the new file inside of the new directory
+  os.chdir(directory)
+  with open (filename, "w") as file:
+    pass
+
+  # Return the list of files in the new directory
+  return os.listdir()
+
+print(new_directory("PythonPrograms", "script.py"))
